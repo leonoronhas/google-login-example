fa32f976abf6d4d582b426925e2a19a1d9d033cb
Leonardo <leosud099@gmail.com>
2020-10-01 21:32:01 -0600

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GoogleLogin from "react-google-login";
+import axios from "axios";
 
 function App() {
+  const responseSuccessGoogle = (response) => {
+    console.log(response);
+    axios({
+      method: "POST",
+      url: "http:localhost:8000/api/googlelogin",
+      data: { tokenId: response.tokenId },
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+  const responseErrorGoogle = (response) => {
+    console.log(response);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="col-md-6 offset-md-3 text-center">
+        <h1>Login with Google Example</h1>
+        <GoogleLogin
+          clientId="67594180334-e3en2jo2k1nq114i1qllpkieu0c7941i.apps.googleusercontent.com"
+          buttonText="Login with Google"
+          onSuccess={responseSuccessGoogle}
+          onFailure={responseErrorGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
     </div>
   );
 }
